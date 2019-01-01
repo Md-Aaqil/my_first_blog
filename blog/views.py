df7c1bcf4af9ccd1d3edf67e72f359ba70530133
Aaqil <mohamedshihabaaqil@gmail.com>
2019-01-01 11:19:20 +0530

    1/1/2019

--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,6 @@ def post_new(request):
         if form.is_valid():
                 post = form.save(commit=False)
                 post.author = request.user
-                post.published_date = timezone.now()
                 post.save()
                 return redirect('post_detail', pk=post.pk)
     else:
@@ -29,9 +28,20 @@ def post_edit(request, pk):
         if form.is_valid():
             posts = form.save(commit=False)
             posts.author = request.user
-            posts.published_date = timezone.now()
             posts.save()
             return redirect('post_detail', pk=posts.pk)
     else:
         form = PostForm(instance=posts)
     return render(request, 'blog/post_edit.html', {'form': form})
+def post_draft_list(request):
+    posts=post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request,'blog/post_draft_list.html',{'posts':posts})
+
+def post_publish(request, pk):
+    posts= get_object_or_404(post, pk=pk)
+    posts.publish()
+    return redirect('post_detail', pk=pk)
+def post_remove(request, pk):
+    posts=get_object_or_404(post, pk=pk)
+    posts.delete()
+    return redirect('post_list')
