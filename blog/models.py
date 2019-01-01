eb535bc7728ced474c6063dd06730db83dde77ac
Md-Aaqil <30803934+Md-Aaqil@users.noreply.github.com>
2019-01-01 16:19:13 +0530

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,6 @@ class post(models.Model):
     text=models.TextField()
     created_date=models.DateTimeField(default=timezone.now)
     published_date=models.DateTimeField(blank=True,null=True)
-    def publish(self):
-        self.published_date=timezone.now()
-        self.save()
+    
     def __str__(self):
         return self.title
