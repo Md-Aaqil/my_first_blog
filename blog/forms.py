0aba689737d4aff6d3905dc1641df41afb1cdbf6
Aaqil <mohamedshihabaaqil@gmail.com>
2019-01-22 19:29:05 +0530

    comment is added

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 
-from .models import post
+from .models import post,Comment
 
 class PostForm(forms.ModelForm):
     class Meta:
         model=post
         fields=('title','text',)
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
-from .models import post
-from .forms import PostForm
+from .models import post,Comment
+from .forms import PostForm,CommentForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 def post_list(request):
@@ -49,3 +49,26 @@ def post_remove(request, pk):
     posts=get_object_or_404(post, pk=pk)
     posts.delete()
     return redirect('post_list')
+def add_comment_to_post(request, pk):
+    posts = get_object_or_404(post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post= posts
+            comment.save()
+            return redirect('post_detail', pk=posts.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
